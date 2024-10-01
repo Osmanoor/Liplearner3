36f9f25411433efcf27f8c1dc2c1611f6539267c
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-10-01 14:40:02 +0200

    Update app.py

--- pretraining/app.py
+++ pretraining/app.py
@@ -4,87 +4,10 @@ from pipeline import *
 import time
 import cv2
 import numpy as np
-from streamlit_webrtc import webrtc_streamer, WebRtcMode
-import av
-import threading
 import os
-# from twilio.rest import Client
-import uuid
-from pathlib import Path
-from aiortc.contrib.media import MediaRecorder
 import ffmpeg
 from crop import *
 
-
-# account_sid = ''
-# auth_token = ''
-# client = Client(account_sid, auth_token)
-
-# token = client.tokens.create()
-
-peer_connection_config = {
-  "iceServers": [
-    {
-      "url": "stun:global.stun.twilio.com:3478",
-      "urls": "stun:global.stun.twilio.com:3478"
-    },
-    {
-      "url": "turn:global.turn.twilio.com:3478?transport=udp",
-      "username": "72ba795b16e54d1d280649db774a72cbd457f45a747f9be8aadd225dae16bc7a",
-      "urls": "turn:global.turn.twilio.com:3478?transport=udp",
-      "credential": "kmAVKEQnf5dNqUeTxqV3zAgLiTJZXrtateM/8cFwROU="
-    },
-    {
-      "url": "turn:global.turn.twilio.com:3478?transport=tcp",
-      "username": "72ba795b16e54d1d280649db774a72cbd457f45a747f9be8aadd225dae16bc7a",
-      "urls": "turn:global.turn.twilio.com:3478?transport=tcp",
-      "credential": "kmAVKEQnf5dNqUeTxqV3zAgLiTJZXrtateM/8cFwROU="
-    },
-    {
-      "url": "turn:global.turn.twilio.com:443?transport=tcp",
-      "username": "72ba795b16e54d1d280649db774a72cbd457f45a747f9be8aadd225dae16bc7a",
-      "urls": "turn:global.turn.twilio.com:443?transport=tcp",
-      "credential": "kmAVKEQnf5dNqUeTxqV3zAgLiTJZXrtateM/8cFwROU="
-    },
-    {
-      "url": "stun:stun.relay.metered.ca:80",
-      "urls": "stun:stun.relay.metered.ca:80",
-    },
-    {
-      "url": "turn:global.relay.metered.ca:80",
-      "urls": "turn:global.relay.metered.ca:80",
-      "username": "3b248090b0c8bb1b18704ca9",
-      "credential": "3YG/B1kIs8fOwRUf",
-    },
-    {
-      "url": "turn:global.relay.metered.ca:80?transport=tcp",
-      "urls": "turn:global.relay.metered.ca:80?transport=tcp",
-      "username": "3b248090b0c8bb1b18704ca9",
-      "credential": "3YG/B1kIs8fOwRUf",
-    },
-    {
-      "url": "turn:global.relay.metered.ca:443",
-      "urls": "turn:global.relay.metered.ca:443",
-      "username": "3b248090b0c8bb1b18704ca9",
-      "credential": "3YG/B1kIs8fOwRUf",
-    },
-    {
-      "url": "turns:global.relay.metered.ca:443?transport=tcp",
-      "urls": "turns:global.relay.metered.ca:443?transport=tcp",
-      "username": "3b248090b0c8bb1b18704ca9",
-      "credential": "3YG/B1kIs8fOwRUf",
-    }
-  ],
-}
-
-def convert_flv_to_mp4(input_file: str, output_file: str):
-    try:
-        stream = ffmpeg.input(input_file)
-        stream = ffmpeg.output(stream, output_file, codec='copy')
-        ffmpeg.run(stream, overwrite_output=True)
-        print(f"Conversion successful: {output_file}")
-    except ffmpeg.Error as e:
-        print(f"An error occurred: {e.stderr.decode()}")
 # Set the page config to use the full width of the screen
 st.set_page_config(layout="wide")
 # Custom CSS for styling
@@ -140,7 +63,7 @@ st.markdown(
 
 # Sidebar with logo and information
 with st.sidebar:
-    st.image("lips.png", width=100)
+    # st.image("lips.png", width=100)
     st.markdown('<div class="title">قارئ الشفاه</div>', unsafe_allow_html=True)
     st.markdown(
         """
@@ -152,38 +75,6 @@ with st.sidebar:
 
 # Main content
 st.markdown('<div class="header">قم بتحميل ملف الفيديو الخاص بك</div>', unsafe_allow_html=True)
-
-# WebtRTC
-RECORD_DIR = Path("./records")
-RECORD_DIR.mkdir(exist_ok=True)
-
-if "prefix" not in st.session_state:
-        st.session_state["prefix"] = str(uuid.uuid4())
-prefix = st.session_state["prefix"]
-in_file = RECORD_DIR / f"{prefix}_input.flv"
-
-def in_recorder_factory() -> MediaRecorder:
-        return MediaRecorder(
-            str(in_file), format="flv"
-        )
-
-webrtc_streamer(
-        key="record",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration=peer_connection_config,
-        media_stream_constraints={
-            "video": True,
-            "audio": False,
-        },
-        in_recorder_factory=in_recorder_factory,
-    )
-
-if in_file.exists():
-    with in_file.open("rb") as f:
-        st.download_button(
-            "Download the recorded video", f, "input.flv"
-        )
-    convert_flv_to_mp4(in_file,"output.mp4")
 
 # Create a file uploader
 uploaded_file = st.file_uploader("", type=["mp4"])
